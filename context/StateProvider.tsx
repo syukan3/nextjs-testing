a26daeff727645c5838a18c3a91550494fd30f5b
栄 俊介 <sakaes@SO-SAKAES-MacBook-Pro.local>
2022-05-05 13:53:23 +0900

    Complete Nextjs

---
+++ context/StateProvider.tsx
@@ -0,0 +1,21 @@
+import { BooleanColumnStatisticsData } from "aws-sdk/clients/glue"
+import { bool } from "aws-sdk/clients/redshiftdata"
+import React, { useContext, useState, createContext } from "react"
+
+const StateContext = createContext(
+    {} as {
+        toggle: boolean
+        setToggle: React.Dispatch<React.SetStateAction<boolean>>
+    }
+)
+
+export const StateProvider: React.FC = ({ children }) => {
+    const [toggle, setToggle] = useState(false)
+  
+    return (
+      <StateContext.Provider value={{ toggle, setToggle }}>
+        {children}
+      </StateContext.Provider>
+    )
+  }
+  export const useStateContext = () => useContext(StateContext)
